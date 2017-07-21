8d01533bce7046ab904722e22baff2cb35b0a116
Masud <mr.masudrana00@gmail.com>
2017-07-21 13:50:03 +0600

    Major updated with bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@
     // All JavaScript pluging initialization code here
 
     
+    // WoW
+	new WOW().init();
 
+	// CounterUp
+	$(".counter").counterUp({
+		delay: 10,
+		time: 1000
+	});
 
-
-
-
+	$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
 
 
 
